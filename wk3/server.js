e9b2428296542382750dcd8aa500a1b7b6fbc5ea
AT <andrei.toma@ie.ase.ro>
2024-11-21 08:24:20 +0200

    .

--- wk3/server.js
+++ wk3/server.js
@@ -20,58 +20,71 @@ try {
 const app = express()
 app.use(express.json())
 
-const cats = []
-let currentCatId = 1
+app.use((req, res, next) => {
+    console.log('middleware called')
+    next()
+})
 
 app.get('/ping', (req, res) => {
     res.send('pong')
 })
 
 // get all cats
-app.get('/cats', (req, res) => {
-    res.status(200).json(cats)
+app.get('/cats', async (req, res, next) => {
+    try {
+        const cats = await Cat.findAll()
+        res.status(200).json(cats)
+    } catch (error) {
+       next(error)
+    }
 }) 
 
 // add a cat
-app.post('/cats', (req, res) => {
-    let newCat = req.body
-    newCat.id = currentCatId
-    cats.push(newCat)
-    currentCatId++
-    res.status(201).json(newCat)
-})
-
-// get a particular cat
-app.get('/cats/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const cat = cats.find(e => e.id === id)
-    if (cat) {
-        res.status(200).json(cat)
-    } else {
-        res.status(404).json({ message: 'not found' })
+app.post('/cats', async (req, res, next) => {
+    try {
+        const cat = await Cat.create(req.body)
+        res.status(201).json(cat)
+    } catch (error) {
+        next(error)
     }
 })
 
-app.put('/cats/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const catIndex = cats.findIndex(e => e.id === id)
-    if (catIndex !== -1) {
-        cats[catIndex].name = req.body.name
-        res.status(202).json(cats[catIndex])
-    } else {
-        res.status(404).json({ message: 'not found' })
-    }
-})
+// // get a particular cat
+// app.get('/cats/:id', (req, res) => {
+//     const id = parseInt(req.params.id)
+//     const cat = cats.find(e => e.id === id)
+//     if (cat) {
+//         res.status(200).json(cat)
+//     } else {
+//         res.status(404).json({ message: 'not found' })
+//     }
+// })
 
-app.delete('/cats/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const catIndex = cats.findIndex(e => e.id === id)
-    if (catIndex !== -1) {
-        cats.splice(catIndex, 1)
-        res.status(204).json({ message: 'deleted' })
-    } else {
-        res.status(404).json({ message: 'not found' })
-    }
+// app.put('/cats/:id', (req, res) => {
+//     const id = parseInt(req.params.id)
+//     const catIndex = cats.findIndex(e => e.id === id)
+//     if (catIndex !== -1) {
+//         cats[catIndex].name = req.body.name
+//         res.status(202).json(cats[catIndex])
+//     } else {
+//         res.status(404).json({ message: 'not found' })
+//     }
+// })
+
+// app.delete('/cats/:id', (req, res) => {
+//     const id = parseInt(req.params.id)
+//     const catIndex = cats.findIndex(e => e.id === id)
+//     if (catIndex !== -1) {
+//         cats.splice(catIndex, 1)
+//         res.status(204).json({ message: 'deleted' })
+//     } else {
+//         res.status(404).json({ message: 'not found' })
+//     }
+// })
+
+app.use((err, req, res, next) => {
+    console.warn(err)
+    res.status(500).json({ message: 'error' })
 })
 
 app.listen(8080)

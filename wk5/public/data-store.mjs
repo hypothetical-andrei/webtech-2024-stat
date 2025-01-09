c02634a86736388dc322cb5eb9df0e0a8623365a
AT <andrei.toma@ie.ase.ro>
2025-01-09 12:34:40 +0200

    .

--- wk5/public/data-store.mjs
+++ wk5/public/data-store.mjs
@@ -28,6 +28,32 @@ class DataStore {
             await this.getAll()
         }
     }
+
+    async saveOne (id, item) {
+        const response = await fetch(`${SERVER}/temperature-records/${id}`, {
+            method: 'put',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(item)
+        })
+        if (!response.ok) {
+            throw response
+        } else {
+            await this.getAll()
+        }
+    }
+
+    async deleteOne (id) {
+        const response = await fetch(`${SERVER}/temperature-records/${id}`, {
+            method: 'delete'
+        })
+        if (!response.ok) {
+            throw response
+        } else {
+            await this.getAll()
+        }
+    }
 }
 
 const store = new DataStore()
